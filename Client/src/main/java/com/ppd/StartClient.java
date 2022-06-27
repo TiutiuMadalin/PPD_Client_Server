875515c4eda64abd3119a4041168721056577c73
TiutiuMadalin <62649591+TiutiuMadalin@users.noreply.github.com>
2022-06-27 19:40:11 +0300

    hasdfas

--- Client/src/main/java/com/ppd/StartClient.java
+++ Client/src/main/java/com/ppd/StartClient.java
@@ -3,7 +3,7 @@ package com.ppd;
 import com.ppd.gui.Gui;
 
 public class StartClient {
-    public static void main(String[] args) throws Exception{
-        Gui.run(args);
+    public static void main(String[] arg) throws Exception{
+        Gui.run(arg);
     }
 }
